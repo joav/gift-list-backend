9f59e5586d2ce53e9ea601ad7565d592f409034a
joav <jim.acero.valcarcel@gmail.com>
2022-07-21 18:20:30 -0500

    Add logs

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import router from "./routes.ts"; // Import our router
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+import logger from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
 
 const PORT = +(Deno.env.get('PORT') || 3000);
 const app = new Application();
+
+app.use(logger.logger);
+app.use(logger.responseTime);
 
 app.use(router.routes()); // Implement our router
 app.use(router.allowedMethods()); // Allow router HTTP methods
