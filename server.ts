d436efea3d8bc09cde820fc2298766e376e33abe
joav <jim.acero.valcarcel@gmail.com>
2022-08-02 10:37:57 -0500

    Add cors

--- server.ts
+++ server.ts
@@ -2,10 +2,13 @@ import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import router from "./routes.ts"; // Import our router
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 import logger from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+
 
 const PORT = +(Deno.env.get('PORT') || 3000);
 const app = new Application();
 
+app.use(oakCors());
 app.use(logger.logger);
 app.use(logger.responseTime);
 
