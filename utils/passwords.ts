9a49bbce3ca76fa74de086bcf7b85fee64c4df2a
joav <jim.acero.valcarcel@gmail.com>
2022-08-02 11:33:39 -0500

    Bcrypt sync

--- utils/passwords.ts
+++ utils/passwords.ts
@@ -1,5 +1,9 @@
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 
-export const createHash = (password: string) => bcrypt.hash(password);
+export const createHash = (password: string) => new Promise<string>((res) => {
+  res(bcrypt.hashSync(password));
+});
 
-export const isValidPassword = (password: string, hash: string) => bcrypt.compare(password, hash);
+export const isValidPassword = (password: string, hash: string) => new Promise<boolean>((res) => {
+  res(bcrypt.compareSync(password, hash));
+});
